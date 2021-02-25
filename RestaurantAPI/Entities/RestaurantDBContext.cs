554040f0c0a837847beb93064bc1d217ec1e32dc
Piotr Dajewski <piterd1998@o2.pl>
2021-02-25 16:12:32 +0100

    Add project files.

---
+++ RestaurantAPI/Entities/RestaurantDBContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDBContext : DbContext
+    {
+	    private string _connectionString =
+		    "Server=DESKTOP-9AH5PNU\\SQLEXPRESS;Database=RestaurantDB;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+		public DbSet<Restaurant> Restaurants { get; set; }
+	    public DbSet<Address> Addresses { get; set; }
+
+	    public DbSet<Dish> Dishes { get; set; }
+
+	    protected override void OnModelCreating(ModelBuilder modelBuilder)
+	    {
+		    modelBuilder.Entity<Restaurant>()
+			    .Property(r => r.Name)
+			    .IsRequired()
+			    .HasMaxLength(25);
+
+		    modelBuilder.Entity<Dish>()
+			    .Property(d => d.Name)
+			    .IsRequired();
+	    }
+
+	    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	    //{
+		   // optionsBuilder.UseSqlServer(_connectionString);
+	    //}
+    }
+}
